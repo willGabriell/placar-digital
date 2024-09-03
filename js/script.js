19909488d738e5b4a585f21059e58089fbb0c404
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-09-03 14:42:38 -0300

    botões de diminuir placar (ainda não finalizados)

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const awayContainer = document.querySelector('#away');
 const homeScore = document.querySelector('#home-score');
 const awayScore = document.querySelector('#away-score');
 
+const homeMinus = document.querySelector('#home-minus');
+const awayMinus = document.querySelector('#away-minus');
+
 let homeCounter = 0;
 
 homeContainer.addEventListener('click', () => {
@@ -12,10 +15,26 @@ homeContainer.addEventListener('click', () => {
     console.log(`Placar: ${homeCounter} X ${awayCounter}`);
 })
 
+homeMinus.addEventListener('click', () => {
+    if (homeCounter > 0) {
+        homeCounter--;
+        homeScore.innerHTML = homeCounter;
+        console.log(`Placar: ${homeCounter} X ${awayCounter}`);
+    }
+})
+
 let awayCounter = 0;
 
 awayContainer.addEventListener('click', () => {
     awayCounter++;
     awayScore.innerHTML = awayCounter;
     console.log(`Placar: ${homeCounter} X ${awayCounter}`);
+})
+
+awayMinus.addEventListener('click', () => {
+    if (awayCounter > 0) {
+        awayCounter--;
+        awayScore.innerHTML = awayCounter;
+        console.log(`Placar: ${homeCounter} X ${awayCounter}`);
+    }
 })
